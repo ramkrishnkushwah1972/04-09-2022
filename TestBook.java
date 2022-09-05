4baa7eb6cdb6b4ef054e6c20f07639ce927f2177
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-05 11:14:40 +0530

    Add files via upload

---
+++ TestBook.java
@@ -0,0 +1,26 @@
+package com.inheritance.pll;
+import com.inheritance.bll.*;
+
+public class TestBook {
+
+public static void main(String[] args) {
+
+		Book b1= new Book();
+		System.out.println(b1);
+		
+		Author A1 = new Author("Yashavant Kanetkar","yas@123.com",'M');
+
+		Book b2= new Book("Letus C", A1,150.0, 2);
+		System.out.println(b2);//b2 == toString()
+		//Author A2 = new Author();
+		System.out.println("**********************************");
+		System.out.println("Name :"+b2.getName());
+	
+		System.out.println("Price :"+b2.getPrice());
+		System.out.println("Quantity :"+b2.getQty());
+
+		//System.out.println("Email :"+ b2.getEmail());
+		
+		
+	}
+}
